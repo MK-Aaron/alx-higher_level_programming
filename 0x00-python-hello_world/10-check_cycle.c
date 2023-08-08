896bf4ddd1cc95093010ccb0e94dbf5d0c047b16
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-07 17:38:02 -0700

    Check cycl;e in singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * check_cycle - infinite loop list
+ * list: pointer to the singly linked list
+ * Return: 0 if no cyle otherwise 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	return (list == NULL ? 0 : 1);
+}
