d3b8aab6e094420a1f8acfb9f5c22dd1f1b7b59c
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-12 03:41:54 +0300

    Betty style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     counter = len(sys.argv) - 1
     if counter == 0:
-        print("0 arguments.") 
+        print("0 arguments.")
     elif counter == 1:
         print("{} argument:".format(counter))
     else:
