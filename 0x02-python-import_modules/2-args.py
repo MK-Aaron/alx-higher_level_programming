0b55d89ad5a152a8c695254eeb55b0b450cd49c1
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-12 03:34:53 +0300

    Betty style Correction of a full stop on output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     counter = len(sys.argv) - 1
     if counter == 0:
-        print("0 arguments") 
+        print("0 arguments.") 
     elif counter == 1:
         print("{} argument:".format(counter))
     else:
         print("{} arguments:".format(counter))
-    for j in range (1, len(sys.argv)):
+    for j in range(1, len(sys.argv)):
             print("{}: {}".format(j, sys.argv[j]))
