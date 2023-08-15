a4c35bb6c0fb6216d70a8d7e98906dbaff38ff45
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-16 02:03:16 +0300

    Make a separate copy list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
+    if my_list != None:
         new_list = my_list.copy()
-        new_list[idx] = element
+    if idx < 0 or idx > len(my_list) - 1:
+        return new_list
+    new_list[idx] = element
     return new_list
