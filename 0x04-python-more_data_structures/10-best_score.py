a95731b11a893c6b7c51b3d5888dbbef50b60bd5
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-17 02:33:26 +0300

    Max value in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    best = max(a_dictionary.values())
+    for item, value in a_dictionary.items():
+        if value == best:
+            return item
