adeea8d39e299c2505a2a6b21b6d2c4a650788ba
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-17 01:02:09 +0300

    pystyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-        new_list = my_list.copy()
-        for i, item in enumerate(my_list):
-            if item == search:
-                new_list[i] = replace
-        return new_list
+    new_list = my_list.copy()
+    for i, item in enumerate(my_list):
+        if item == search:
+            new_list[i] = replace
+    return new_list
