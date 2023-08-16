fd8eb9a02e7aa9a31476f83a6258b569b32334ca
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-08-16 21:26:52 +0000

    Square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return matrix
+
+    new_matrix = [square_array(r.copy()) for r in matrix]
+    return new_matrix
+
+def square_array(arr):
+    return [i**2 for i in arr]
