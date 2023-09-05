333a7df4b50b70db100c9fbc5710ffa17bfb6151
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-09-05 15:08:18 -0700

    Correction of pycodestyle and typos

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,9 @@ number = random.randint(-10000, 10000)
 digit = abs(number) % 10
 
 if digit > 5:
-    print(f"The last digit of {number:d} is {digit:d} greater than 5")
+    print(f"Last digit of {number:d} is {digit:d} and is greater than 5")
 elif digit == 0:
-    print(f"The last digit of {number:d} is {digit:d} and is 0")
-#elif digit < 6 and digit != 0:
-else:
-    print(f"The last digit of {number:d} is {digit:d} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {digit:d} and is 0")
+elif digit < 6 and digit != 0:
+    print(f"Last digit of {number:d} is {digit:d} and\
+            is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,5 @@
 for item in range(97, 123):
     if item == 101 or item == 113:
         continue
-    print(f"{item:c}", end="")
+    else:
+        print(chr(item), end ='')
