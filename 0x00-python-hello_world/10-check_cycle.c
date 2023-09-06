068e90260673bda5bc95095e21911033fffddb66
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-09-06 05:12:16 -0700

    Check if list is valid

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,14 +9,18 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
-	slow = list;
-	fast = list->next->next;
-	while (slow && slow->next && fast->next->next)
+	if (list != NULL)
 	{
-		if (slow == fast)
-			return (1);
-		slow = slow->next;
-		fast = fast->next->next;
+		slow = list;
+		fast = list->next;
+	
+		while (slow && slow->next && fast->next->next)
+		{
+			if (slow == fast)
+				return (1);
+			slow = slow->next;
+			fast = fast->next->next;
+		}
 	}
 	return (0);
 }
