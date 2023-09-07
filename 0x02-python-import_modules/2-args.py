e6e093635fc9ece3dab8e5db385b7b904a70f394
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-09-07 03:08:16 -0700

    Guard from import as module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import sys
-arg = len(sys.argv)
 
-if arg == 1:
-    print(f"0 arguments.")
-else:
-    print(f"{arg - 1} argument", end='')
-    if arg == 2:
-        print(":")
+if __name__ == "__main__":
+    arg = len(sys.argv)
+
+    if arg == 1:
+        print(f"0 arguments.")
     else:
-        print("s:")
-    for i in range(1, arg):
-        print("{}: {}".format(i, sys.argv[i]))
+        print(f"{arg - 1} argument", end='')
+        if arg == 2:
+            print(":")
+        else:
+            print("s:")
+        for i in range(1, arg):
+            print("{}: {}".format(i, sys.argv[i]))
