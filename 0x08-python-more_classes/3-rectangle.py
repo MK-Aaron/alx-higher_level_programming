8eb452d8ccc4e02dea37c075bcf0c488c7ad7cb5
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-10-02 07:21:04 -0700

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
+class Rectangle:
+    """class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Intializing the rectangle
+
+        Args:
+            width (int): The width of the new rectangle
+            height (int): Height of new rectangle
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Checks if value width is valid"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/Set height of rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Check if value width is valid"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns area of the reectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Return representation of a rectangle using '#'"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        loop = []
+        for i in range(self.__height):
+            [loop.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                loop.append("\n")
+        return ("".join(loop))
