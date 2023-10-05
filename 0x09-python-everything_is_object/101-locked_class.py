38f269f2ea4e14c857532b0a35061e6bf24c7d1b
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-10-04 18:27:13 -0700

    Everything is python

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,10 @@
 #!/usr/bin/pythonn3
 """Class LockedClass with no class or attribute"""
 
-    
+
+class LockedClass:
+    """Prevent user from creating other instance
+
+    crete only for "firstname"""
+
+    __slots__ = ["first_name"]
