9da22b49458d12e9ec53c3fc683502be5d073b23
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-10-10 05:40:05 -0700

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Function that writes a string to
+"""
+Function that writes a string to
+a text file
 
-a text file"""
+"""
 
 
 def write_file(filename="", text=""):
@@ -9,16 +11,4 @@ def write_file(filename="", text=""):
 
     with open(filename, "w", encoding="utf-8") as ptr:
         ptr.write(text)
-
-    with open(filename, encoding="utf-8") as ptr:
-        line = ptr.read()
-
-    wordList = line.split()
-
-    charCount = 0
-
-    for word in wordList:
-        for char in word:
-            charCount += 1
-
-    return charCount + (len(wordList) - 1)
+        return ptr.tell()
