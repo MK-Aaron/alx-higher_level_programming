f90110b0b461625d4ec752cd2447b8c6268b986f
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-10-13 23:41:20 -0700

    Add araea method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -115,3 +115,10 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        Returns:
+            Area of a rectangle - width * height
+        """
+        return self.__width * self.__height
