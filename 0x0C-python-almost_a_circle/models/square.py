c592844959eb2d4ef46dc7bbaa18fad29a60d26f
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-10-16 01:04:35 -0700

    add square glass

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Model square inherits from rectangle
+
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class  square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Intialize the square
+
+        Args:
+            size (int): width or height are equal in a square
+            x (int): x axis
+            y (int): y axis
+            id (list): identity of the square
+        """
+        super().__init__(id=id, width=size, height=size, x=x, y=y)
+
+    def __str__(self):
+        """returns the string representation of dimentions"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
