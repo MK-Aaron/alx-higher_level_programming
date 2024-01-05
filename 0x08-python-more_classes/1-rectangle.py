e490ee474efbe0ef6a570084885c114d0a3c68c4
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-01-04 17:05:45 -0800

    Real defination of a rectabgle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,24 +6,36 @@ class Rectangle:
     """Class rectangle"""
     def __init__(self, width=0, height=0):
         """An instance in a class"""
-        self.__width = width
         self.__height = height
+        self.__width = width
 
-    def width(self, value):
+    @property
+    def width(self):
         """Get width value"""
-        if width is None or type(width) not in [int]:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        return self.width
+        return self.__width
 
+    @width.setter
+    def width(self, value):
+        """Confirm width value"""
+        if not isinstance (value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height value"""
+        return self.__height
+
+    @height.setter
     def height(self, value):
         """Get height value"""
-        if height is None or type(height) not in [int]:
+        if not isinstance (value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        return self.height
+        self.__height = value
 
     def __rpr__(self):
         """Returns dimentions of object"""
