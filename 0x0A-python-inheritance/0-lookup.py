27bbe45c15c393bd76fc4b5de9b6e4fe2366d295
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-01-08 16:03:58 -0800

    update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An example of function that returns
+list of arguements"""
+
+
+def lookup(obj):
+    """Function that returns list
+    of availale attributes"""
+    return dir(obj)
