de6665d6a6cc018c238ae6099587d3642ed06d5f
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-01-08 16:02:19 -0800

    update

--- 0-lookup.py/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""An example of function that returns
-list of arguements"""
-
-
-def lookup(obj):
-    """Function that returns list
-    of availale attributes"""
-    return dir(obj)
